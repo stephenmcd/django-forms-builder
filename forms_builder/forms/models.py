da3feae4217c8d86e886cb82b697dc7dfbaefb46
stephenmcd <steve@jupo.org>
2010-07-29 07:21:06 +1000

    Convert tabs to spaces.

--- forms_builder/forms/models.py
+++ forms_builder/forms/models.py
@@ -23,50 +23,50 @@ FIELD_CHOICES = (
 )
 
 class Form(models.Model):
-	"""
-	A user-built form.
-	"""
+    """
+    A user-built form.
+    """
 
-	class Meta:
-		verbose_name = _("Form")
-		verbose_name_plural = _("Forms")
+    class Meta:
+        verbose_name = _("Form")
+        verbose_name_plural = _("Forms")
 
-	title = models.CharField(_("Title"), max_length=50, unique=True)
-	slug = models.SlugField(editable=False, max_length=100, unique=True)
-	intro = models.TextField(_("Intro"), max_length=2000)
-	response = models.TextField(_("Response"), max_length=2000)
-	status = models.IntegerField(_("Status"), choices=STATUS_CHOICES, 
-	    default=STATUS_PUBLIC)
-	send_email = models.BooleanField(_("Send email response"), default=True)
-	send_copy = models.CharField(_("Send copies to"), max_length=200, 
-	    help_text=_("One or more email addresses, separated by commas"), 
-	    blank=True)
-	
-	def __unicode__(self):
-		return self.title
+    title = models.CharField(_("Title"), max_length=50, unique=True)
+    slug = models.SlugField(editable=False, max_length=100, unique=True)
+    intro = models.TextField(_("Intro"), max_length=2000)
+    response = models.TextField(_("Response"), max_length=2000)
+    status = models.IntegerField(_("Status"), choices=STATUS_CHOICES, 
+        default=STATUS_PUBLIC)
+    send_email = models.BooleanField(_("Send email response"), default=True)
+    send_copy = models.CharField(_("Send copies to"), max_length=200, 
+        help_text=_("One or more email addresses, separated by commas"), 
+        blank=True)
+    
+    def __unicode__(self):
+        return self.title
 
-	def save(self, *args, **kwargs):
-		"""
-		Create a unique slug from title - append an index and increment if it 
-		already exists.
-		"""
+    def save(self, *args, **kwargs):
+        """
+        Create a unique slug from title - append an index and increment if it 
+        already exists.
+        """
 
-		if not self.slug:
-			slug = temp = slugify(self.title)
-			index = 0
-			while True:
-				try:
-					Form.objects.get(slug=temp)
-				except Form.DoesNotExist:
-					break
-				index += 1
-				temp = "%s-%s" (slug, index)
-			self.slug = temp
-		super(Form, self).save(*args, **kwargs)
-		
-	@models.permalink
-	def get_absolute_url(self):
-		return ("form_detail", (), {"slug": self.slug})
+        if not self.slug:
+            slug = temp = slugify(self.title)
+            index = 0
+            while True:
+                try:
+                    Form.objects.get(slug=temp)
+                except Form.DoesNotExist:
+                    break
+                index += 1
+                temp = "%s-%s" (slug, index)
+            self.slug = temp
+        super(Form, self).save(*args, **kwargs)
+        
+    @models.permalink
+    def get_absolute_url(self):
+        return ("form_detail", (), {"slug": self.slug})
 
 class Field(models.Model):
     """
