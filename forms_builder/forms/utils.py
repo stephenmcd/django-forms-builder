cda1e5189375007a8a450943b0a3f139cf2e9a0b
stephenmcd <steve@jupo.org>
2010-07-29 07:21:06 +1000

    Convert tabs to spaces.

--- forms_builder/forms/utils.py
+++ forms_builder/forms/utils.py
@@ -5,10 +5,10 @@ from forms_builder.forms.models import Form, STATUS_PUBLIC
 
 
 def get_form(request, slug):
-	"""
-	Wrap get_object_or_404 with a check against status for non-staff users.
-	"""
-	queryset = Form.objects.all()
-	if not request.user.is_staff:
-		queryset = queryset.filter(status=STATUS_PUBLIC)
-	return get_object_or_404(queryset, slug=slug)
+    """
+    Wrap get_object_or_404 with a check against status for non-staff users.
+    """
+    queryset = Form.objects.all()
+    if not request.user.is_staff:
+        queryset = queryset.filter(status=STATUS_PUBLIC)
+    return get_object_or_404(queryset, slug=slug)
--- forms_builder/forms/views.py
+++ forms_builder/forms/views.py
@@ -15,24 +15,24 @@ from forms_builder.forms.settings import UPLOAD_TO, EMAIL_TO
 
 
 def form_detail(request, slug, template="forms/form_detail.html"):
-	"""
-	Display a built form and handle submission.
-	"""
-	
-	form = get_form(request, slug)
-	form_for_form = FormForForm(request.POST or None, form=form)
+    """
+    Display a built form and handle submission.
+    """
+    
+    form = get_form(request, slug)
+    form_for_form = FormForForm(request.POST or None, form=form)
 
-	if request.method == "POST":
-		if form_for_form.is_valid():
-			return redirect(reverse("form_sent", kwargs={"slug": form.slug}))
+    if request.method == "POST":
+        if form_for_form.is_valid():
+            return redirect(reverse("form_sent", kwargs={"slug": form.slug}))
 
-	context = {"form": form, "form_for_form": form_for_form}
-	return render_to_response(template, context, RequestContext(request))
+    context = {"form": form, "form_for_form": form_for_form}
+    return render_to_response(template, context, RequestContext(request))
 
 
 def form_sent(request, slug, template="forms/form_sent.html"):
-	"""
-	Show the response message.
-	"""
-	context = {"form": get_form(request, slug)}
-	return render_to_response(template, context, RequestContext(request))
+    """
+    Show the response message.
+    """
+    context = {"form": get_form(request, slug)}
+    return render_to_response(template, context, RequestContext(request))
