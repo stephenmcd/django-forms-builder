20fe4a6dc88516b3248da791acd4894a1eaf7e95
stephenmcd <steve@jupo.org>
2010-07-30 07:52:26 +1000

    Refined admin class.

--- forms_builder/forms/admin.py
+++ forms_builder/forms/admin.py
@@ -1,5 +1,6 @@
 
 from django.contrib import admin
+from django.utils.translation import ugettext_lazy as _
 
 from forms_builder.forms.models import Form, Field
 
@@ -9,7 +10,16 @@ class FieldAdmin(admin.TabularInline):
 
 class FormAdmin(admin.ModelAdmin):
     inlines = (FieldAdmin,)
-
+    list_display = ("title","status","email_from","email_copies","admin_link")
+    list_display_links = ("title",)
+    list_editable = ("status","email_from","email_copies")
+    list_filter = ("status",)
+    search_fields = ("title","intro","response","email_from","email_copies")
+    radio_fields = {"status": admin.HORIZONTAL}
+    fieldsets = (
+        (None, {"fields": ("title", "status", "intro", "response")}),
+        (_("Email"), {"fields": ("send_email", "email_from", "email_copies")}),
+    )
 
 admin.site.register(Form, FormAdmin)
 
