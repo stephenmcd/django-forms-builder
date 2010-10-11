eea630770bd833b0feded60b6e3100e0d923886e
Chris Heisel <chris@heisel.org>
2010-10-11 12:25:10 -0400

    Moving default site functionality into a callable to prevent syncdb/test/other management command issues

--- forms_builder/forms/models.py
+++ forms_builder/forms/models.py
@@ -15,27 +15,25 @@ from forms_builder.forms.settings import FIELD_MAX_LENGTH, LABEL_MAX_LENGTH, \
 STATUS_DRAFT = 1
 STATUS_PUBLISHED = 2
 STATUS_CHOICES = (
-    (STATUS_DRAFT, "Draft"), 
+    (STATUS_DRAFT, "Draft"),
     (STATUS_PUBLISHED, "Published"),
 )
 
+
+
 sites_field = None
 if USE_SITES:
-    import sys
-    from django.contrib.sites.models import Site
-    kwargs = {}
-    if not (len(sys.argv) >= 2 and sys.argv[1] == "syncdb"):
-        sites = Site.objects.all()
-        if len(sites) == 1:
-            kwargs["default"] = (sites[0].id,)
-    sites_field = models.ManyToManyField(Site, **kwargs)
+    def default_site():
+        return Site.objects.get_current_site()
+
+    sites_field = models.ManyToManyField(Site, default=default_site)
 
 FIELD_CHOICES = (
     ("CharField", _("Single line text")),
     ("CharField/django.forms.Textarea", _("Multi line text")),
     ("EmailField", _("Email")),
     ("BooleanField", _("Check box")),
-    ("MultipleChoiceField/django.forms.CheckboxSelectMultiple", 
+    ("MultipleChoiceField/django.forms.CheckboxSelectMultiple",
         _("Check boxes")),
     ("ChoiceField", _("Drop down")),
     ("MultipleChoiceField", _("Multi select")),
@@ -53,8 +51,8 @@ class FormManager(models.Manager):
     def published(self, for_user=None):
         if for_user is not None and for_user.is_staff:
             return self.all()
-        return self.filter( 
-            Q(publish_date__lte=datetime.now()) | Q(publish_date__isnull=True), 
+        return self.filter(
+            Q(publish_date__lte=datetime.now()) | Q(publish_date__isnull=True),
             Q(expiry_date__gte=datetime.now()) | Q(expiry_date__isnull=True),
             Q(status=STATUS_PUBLISHED))
 
@@ -75,25 +73,25 @@ class AbstractForm(models.Model):
     title = models.CharField(_("Title"), max_length=50)
     slug = models.SlugField(editable=False, max_length=100, unique=True)
     intro = models.TextField(_("Intro"))
-    button_text = models.CharField(_("Button text"), max_length=50, 
+    button_text = models.CharField(_("Button text"), max_length=50,
         default=_("Submit"))
     response = models.TextField(_("Response"))
-    status = models.IntegerField(_("Status"), choices=STATUS_CHOICES, 
+    status = models.IntegerField(_("Status"), choices=STATUS_CHOICES,
         default=STATUS_PUBLISHED)
-    publish_date = models.DateTimeField(_("Published from"), 
+    publish_date = models.DateTimeField(_("Published from"),
         help_text=_("With published selected, won't be shown until this time"),
         blank=True, null=True)
-    expiry_date = models.DateTimeField(_("Expires on"), 
+    expiry_date = models.DateTimeField(_("Expires on"),
         help_text=_("With published selected, won't be shown after this time"),
         blank=True, null=True)
-    login_required = models.BooleanField(_("Login required"), 
+    login_required = models.BooleanField(_("Login required"),
         help_text=_("If checked, only logged in users can view the form"))
     send_email = models.BooleanField(_("Send email"), default=True, help_text=
         _("If checked, the person entering the form will be sent an email"))
-    email_from = models.EmailField(_("From address"), blank=True, 
+    email_from = models.EmailField(_("From address"), blank=True,
         help_text=_("The address the email will be sent from"))
-    email_copies = models.CharField(_("Send copies to"), blank=True, 
-        help_text=_("One or more email addresses, separated by commas"), 
+    email_copies = models.CharField(_("Send copies to"), blank=True,
+        help_text=_("One or more email addresses, separated by commas"),
         max_length=200)
     email_subject = models.CharField(_("Subject"), max_length=200, blank=True)
     email_message = models.TextField(_("Message"), blank=True)
@@ -104,13 +102,13 @@ class AbstractForm(models.Model):
         verbose_name = _("Form")
         verbose_name_plural = _("Forms")
         abstract = True
-    
+
     def __unicode__(self):
         return self.title
 
     def save(self, *args, **kwargs):
         """
-        Create a unique slug from title - append an index and increment if it 
+        Create a unique slug from title - append an index and increment if it
         already exists.
         """
         if not self.slug:
@@ -125,7 +123,7 @@ class AbstractForm(models.Model):
                     break
                 i += 1
         super(AbstractForm, self).save(*args, **kwargs)
-        
+
     @models.permalink
     def get_absolute_url(self):
         return ("form_detail", (), {"slug": self.slug})
@@ -141,7 +139,7 @@ class AbstractForm(models.Model):
         return "<a href='%s'>%s</a>" % (url, ugettext("Export entries"))
     admin_link_export.allow_tags = True
     admin_link_export.short_description = ""
-    
+
 class FieldManager(models.Manager):
     """
     Only show visible fields when displaying actual form..
@@ -153,35 +151,35 @@ class AbstractField(models.Model):
     """
     A field for a user-built form.
     """
-    
+
     label = models.CharField(_("Label"), max_length=LABEL_MAX_LENGTH)
-    field_type = models.CharField(_("Type"), choices=FIELD_CHOICES, 
+    field_type = models.CharField(_("Type"), choices=FIELD_CHOICES,
         max_length=55)
     required = models.BooleanField(_("Required"), default=True)
     visible = models.BooleanField(_("Visible"), default=True)
-    choices = models.CharField(_("Choices"), max_length=1000, blank=True, 
+    choices = models.CharField(_("Choices"), max_length=1000, blank=True,
         help_text="Comma separated options where applicable. If an option "
             "itself contains commas, surround the option starting with the %s"
-            "character and ending with the %s character." % 
+            "character and ending with the %s character." %
                 (CHOICES_QUOTE, CHOICES_UNQUOTE))
-    default = models.CharField(_("Default value"), blank=True, 
+    default = models.CharField(_("Default value"), blank=True,
         max_length=FIELD_MAX_LENGTH)
     help_text = models.CharField(_("Help text"), blank=True, max_length=100)
-        
+
     objects = FieldManager()
 
     class Meta:
         verbose_name = _("Field")
         verbose_name_plural = _("Fields")
         abstract = True
-    
+
     def __unicode__(self):
         return self.label
 
     def get_choices(self):
         """
-        Parse a comma separated choice string into a list of choices taking 
-        into account quoted choices using the ``CHOICES_QUOTE`` and 
+        Parse a comma separated choice string into a list of choices taking
+        into account quoted choices using the ``CHOICES_QUOTE`` and
         ``CHOICES_UNQUOTE`` settings.
         """
         choice = ""
@@ -208,17 +206,17 @@ class AbstractFormEntry(models.Model):
     """
 
     entry_time = models.DateTimeField(_("Date/time"))
-    
+
     class Meta:
         verbose_name = _("Form entry")
         verbose_name_plural = _("Form entries")
         abstract = True
-    
+
 class AbstractFieldEntry(models.Model):
     """
     A single field value for a form entry submitted via a user-built form.
     """
-    
+
     field_id = models.IntegerField()
     value = models.CharField(max_length=FIELD_MAX_LENGTH)
 
