155da2eb2f80e4f38a348ec91ab660da2a2445b8
stephenmcd <steve@jupo.org>
2010-12-02 16:23:57 +1100

    Added initial optional support for HTML5 with placeholder attributes.

--- forms_builder/forms/settings.py
+++ forms_builder/forms/settings.py
@@ -11,13 +11,15 @@ LABEL_MAX_LENGTH = getattr(settings, "FORMS_BUILDER_LABEL_MAX_LENGTH", 200)
 # The absolute path where files will be uploaded to.
 UPLOAD_ROOT = getattr(settings, "FORMS_BUILDER_UPLOAD_ROOT", None)
 
+# Boolean controlling whether HTML5 features are used.
+USE_HTML5 = getattr(settings, "FORMS_BUILDER_USE_HTML5", True)
+
 # Boolean controlling whether forms are associated to Django's Sites framework.
 USE_SITES = getattr(settings, "FORMS_BUILDER_USE_SITES", 
-    "django.contrib.sites" in settings.INSTALLED_APPS)
+                    "django.contrib.sites" in settings.INSTALLED_APPS)
 
 # Char to start a quoted choice with.
 CHOICES_QUOTE = getattr(settings, "FORMS_BUILDER_CHOICES_QUOTE", "`")
 
 # Char to end a quoted choice with.
 CHOICES_UNQUOTE = getattr(settings, "FORMS_BUILDER_CHOICES_UNQUOTE", "`")
-
