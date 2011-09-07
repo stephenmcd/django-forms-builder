2e9cd362d7d29d42f63be30e4e414ec7741fa5b7
stephenmcd <steve@jupo.org>
2011-09-08 06:14:13 +1000

    Bumped the version requirement for sphinx-me.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     zip_safe = False,
     include_package_data = True,
     packages = find_packages(),
-    install_requires=["sphinx-me",],
+    install_requires=["sphinx-me >= 0.1.2",],
     classifiers = [
         "Development Status :: 5 - Production/Stable",
         "Environment :: Web Environment",
