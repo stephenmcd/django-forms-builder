f22f8ccab95674b2e40801b687440063d23922f7
Sindre Sorhus <mofle89@gmail.com>
2011-10-05 14:28:24 +0200

    Add Unicode support to the CSV writer. Fixes #29.

--- forms_builder/forms/admin.py
+++ forms_builder/forms/admin.py
@@ -18,6 +18,7 @@ from django.utils.translation import ugettext_lazy as _
 from forms_builder.forms.forms import ExportForm
 from forms_builder.forms.models import Form, Field, FieldEntry
 from forms_builder.forms.settings import CSV_DELIMITER, UPLOAD_ROOT, USE_SITES
+from forms_builder.forms.utils import UnicodeWriter
 
 
 fs = FileSystemStorage(location=UPLOAD_ROOT)
@@ -89,10 +90,10 @@ class FormAdmin(admin.ModelAdmin):
                 response = HttpResponse(mimetype="text/csv")
                 fname = "%s-%s.csv" % (form.slug, slugify(datetime.now().ctime()))
                 response["Content-Disposition"] = "attachment; filename=%s" % fname
-                csv = writer(response, delimiter=CSV_DELIMITER)
+                csv = UnicodeWriter(response, delimiter=CSV_DELIMITER)
                 csv.writerow(export_form.columns())
-                for rows in export_form.rows(csv=True):
-                    csv.writerow(rows)
+                for row in export_form.rows(csv=True):
+                    csv.writerow(row)
                 return response
         template = "admin/forms/export.html"
         context = {"title": _("Export Entries"), "export_form": export_form,
---
+++ forms_builder/forms/utils.py
@@ -0,0 +1,31 @@
+import csv, codecs, cStringIO
+
+class UnicodeWriter:
+    """
+    http://docs.python.org/library/csv.html
+    A CSV writer which will write rows to CSV file "f",
+    which is encoded in the given encoding.
+    """
+
+    def __init__(self, f, dialect=csv.excel, encoding="utf-8", **kwds):
+        # Redirect output to a queue
+        self.queue = cStringIO.StringIO()
+        self.writer = csv.writer(self.queue, dialect=dialect, **kwds)
+        self.stream = f
+        self.encoder = codecs.getincrementalencoder(encoding)()
+
+    def writerow(self, row):
+        self.writer.writerow([s.encode("utf-8") if isinstance(s, basestring) else s for s in row])
+        # Fetch UTF-8 output from the queue ...
+        data = self.queue.getvalue()
+        data = data.decode("utf-8")
+        # ... and reencode it into the target encoding
+        data = self.encoder.encode(data)
+        # write to the target stream
+        self.stream.write(data)
+        # empty queue
+        self.queue.truncate(0)
+
+    def writerows(self, rows):
+        for row in rows:
+            self.writerow(row)
