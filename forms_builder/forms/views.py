5072f701eee14124f0b6ae7cbe7e8e0287f80617
Toby White <tow@eaddrinu.se>
2012-01-02 14:08:56 +0000

    And fix view up to remove unnecessary processing

--- forms_builder/forms/views.py
+++ forms_builder/forms/views.py
@@ -21,25 +21,5 @@ def form_detail(request, slug, template="forms/form_detail.html"):
     except Form.objects.LoginRequired:
         return redirect("%s?%s=%s" % (settings.LOGIN_URL, REDIRECT_FIELD_NAME,
                         urlquote(request.get_full_path())))
-    if request.method == "POST":
-        entry, form_for_form = form.process_form(request)
-        if entry:
-            return redirect(reverse("form_sent", kwargs={"slug": form.slug}))
-    else:
-        form_for_form = form.form_for_form()
-    context = {"form": form, "form_for_form": form_for_form}
-    return render_to_response(template, context, RequestContext(request))
-
-def form_sent(request, slug, template="forms/form_sent.html"):
-    """
-    Show the response message.
-    """
-    try:
-        form = Form.objects.get_form_for_request(request, slug)
-    except Form.DoesNotExist:
-        raise Http404
-    except Form.objects.LoginRequired:
-        return redirect("%s?%s=%s" % (settings.LOGIN_URL, REDIRECT_FIELD_NAME,
-                        urlquote(request.get_full_path())))
     context = {"form": form}
     return render_to_response(template, context, RequestContext(request))
