83b822079eec985a43c72e8a1e7f8ce645029920
CNBorn <CNBorn@hotmail.com>
2012-06-28 14:53:22 +0800

    using Mezzanine's slugify, which doesn't require extra dependency

--- forms_builder/forms/slugify.py
+++ forms_builder/forms/slugify.py
@@ -1,9 +1,18 @@
-from django.template.defaultfilters import slugify as generic_slugify
+import re
+import unicodedata
+from django.utils.encoding import smart_unicode
 
-def slugify(input_text):
-    try:
-        from unidecode import unidecode
-        slug = generic_slugify(unidecode(input_text))
-    except ImportError:
-        slug = generic_slugify(input_text)
-    return slug
+def slugify(s):
+    """
+    Replacement for Django's slugify which allows unicode chars in
+    slugs, for URLs in Chinese, Russian, etc.
+    Adopted from https://github.com/mozilla/unicode-slugify/
+    """
+    chars = []
+    for char in smart_unicode(s):
+        cat = unicodedata.category(char)[0]
+        if cat in "LN" or char in "-_~":
+            chars.append(char)
+        elif cat == "Z":
+            chars.append(" ")
+    return re.sub("[-\s]+", "-", "".join(chars).strip()).lower()
