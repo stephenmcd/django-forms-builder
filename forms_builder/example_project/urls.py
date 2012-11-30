590ef8b6b74935ab65f7556b55220bbc3e5712f1
Stephen McDonald <steve@jupo.org>
2012-11-30 17:51:44 +1100

    RIP generic views, stung to death by classes.

--- forms_builder/example_project/urls.py
+++ forms_builder/example_project/urls.py
@@ -1,7 +1,7 @@
 
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
-from django.views.generic import TemplateView
+from django.shortcuts import render
 
 from forms_builder.forms.models import Form
 from forms_builder.forms import urls as form_urls
@@ -12,6 +12,6 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^forms/', include(form_urls)),
-    url(r'^$', lambda request: TemplateView.as_view(
-        template_name="index.html")(request, forms=Form.objects.all())),
+    url(r'^$', lambda request: render(request, "index.html",
+                                      {"forms": Form.objects.all()})),
 )
