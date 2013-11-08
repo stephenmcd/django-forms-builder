890015ea46365b8d801563d98ae20a91a4ca8bce
Stephen McDonald <steve@jupo.org>
2013-11-09 09:46:07 +1100

    Remove old manage.py code in example project

--- forms_builder/example_project/manage.py
+++ forms_builder/example_project/manage.py
@@ -2,18 +2,14 @@
 import sys
 import os
 
+from settings import PROJECT_ROOT, PROJECT_DIRNAME
+
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '../..'))
-
-from django.core.management import execute_manager
-import imp
-try:
-    imp.find_module('settings') # Assumed to be in the same directory.
-except ImportError:
-    import sys
-    sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n" % __file__)
-    sys.exit(1)
-
-import settings
+os.chdir(PROJECT_ROOT)
+sys.path.insert(0, os.path.abspath(os.path.join(PROJECT_ROOT, "..")))
 
 if __name__ == "__main__":
-    execute_manager(settings)
+    settings_module = "%s.settings" % PROJECT_DIRNAME
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
+    from django.core.management import execute_from_command_line
+    execute_from_command_line(sys.argv)
