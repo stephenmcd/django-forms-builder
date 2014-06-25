ad6a7738fd3cdd903a59bb281420f74b01555c1d
Stephen McDonald <steve@jupo.org>
2014-06-26 08:02:30 +1000

    redirect_url belongs with response in admin

--- forms_builder/forms/admin.py
+++ forms_builder/forms/admin.py
@@ -36,8 +36,7 @@ form_admin_filter_horizontal = ()
 form_admin_fieldsets = [
     (None, {"fields": ("title", ("status", "login_required",),
         ("publish_date", "expiry_date",),
-        "intro", "button_text", "response")}),
-    (_('Redirection'), {'fields': ('redirect_url',), 'classes': ('collapse',)}),
+        "intro", "button_text", "response", "redirect_url")}),
     (_("Email"), {"fields": ("send_email", "email_from", "email_copies",
         "email_subject", "email_message")}),]
 
