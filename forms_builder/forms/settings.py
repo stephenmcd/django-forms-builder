110c0cc0d40c4731440d2b3f58ac178a866451ff
Stephen McDonald <steve@jupo.org>
2015-02-27 19:04:21 +1100

    Raise a clearer error if the sites app is missing. Closes #154.

--- forms_builder/forms/settings.py
+++ forms_builder/forms/settings.py
@@ -1,6 +1,12 @@
 from __future__ import unicode_literals
 
 from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
+
+
+if not (getattr(settings, "SITE_ID") and
+        "django.contrib.sites" in settings.INSTALLED_APPS):
+    raise ImproperlyConfigured("django.contrib.sites is required")
 
 
 # The maximum allowed length for field values.
@@ -22,8 +28,7 @@ UPLOAD_ROOT = getattr(settings, "FORMS_BUILDER_UPLOAD_ROOT", None)
 USE_HTML5 = getattr(settings, "FORMS_BUILDER_USE_HTML5", True)
 
 # Boolean controlling whether forms are associated to Django's Sites framework.
-USE_SITES = getattr(settings, "FORMS_BUILDER_USE_SITES",
-                    "django.contrib.sites" in settings.INSTALLED_APPS)
+USE_SITES = getattr(settings, "FORMS_BUILDER_USE_SITES", False)
 
 # Boolean controlling whether form slugs are editable in the admin.
 EDITABLE_SLUGS = getattr(settings, "FORMS_BUILDER_EDITABLE_SLUGS", False)
