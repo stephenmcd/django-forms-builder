ee3edf3834ee5bb764e3e7948daa1955d8d4fc5a
annabrodersen <36651859+annabrodersen@users.noreply.github.com>
2019-07-24 15:44:46 -0500

    revert to original

--- forms_builder/forms/views.py
+++ forms_builder/forms/views.py
@@ -119,5 +119,5 @@ def form_sent(request, slug, template="forms/form_sent.html"):
     Show the response message.
     """
     published = Form.objects.published(for_user=request.user)
-    context = RequestContext(request, {"form": form})
+    context = {"form": get_object_or_404(published, slug=slug)}
     return render(request, template, context=context)
