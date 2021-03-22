c57105d6bbd4e956433a0ffd000871a3b10fb42c
Matt Purnell <matt.purnell98@gmail.com>
2021-03-21 22:26:06 -0500

    Last few fixes for tests

--- forms_builder/example_project/settings.py
+++ forms_builder/example_project/settings.py
@@ -75,6 +75,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
+    'django.contrib.messages',
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.staticfiles',
--- forms_builder/example_project/urls.py
+++ forms_builder/example_project/urls.py
@@ -1,13 +1,13 @@
 from __future__ import unicode_literals
 
-from django.conf.urls import url
-from django.contrib import admin
-from django.shortcuts import render
 try:
     from django.urls import include
 except ImportError:
     # For Django 1.8 compatibility
     from django.conf.urls import include
+from django.conf.urls import url
+from django.contrib import admin
+from django.shortcuts import render
 
 from forms_builder.forms.models import Form
 from forms_builder.forms import urls as form_urls
